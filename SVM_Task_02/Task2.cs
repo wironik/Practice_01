a8c9f2d69ac4c012b969aa747c7ebff0621a4e6c
wironik_z <73844705+wironik@users.noreply.github.com>
2020-11-05 16:11:48 +0300

    SVM_Task_02

---
+++ SVM_Task_02/Task2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SVM_Task_02
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            string exit=" ";
+            Console.WriteLine("Данная программа будет выполняться до тех пор, пока не будет введено слово exit");
+            while (exit!="exit")
+            {
+                exit = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Вы ввели: " + exit);
+            }
+        }
+    }
+}
