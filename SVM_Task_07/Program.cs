cdc150b3e33766e9adb4a758c6dfee5071f01455
wironik_z <73844705+wironik@users.noreply.github.com>
2020-11-06 18:46:57 +0300

    SVM_Task_07

---
+++ SVM_Task_07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SVM_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            int[] NewArr = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++) arr[i] = rnd.Next(0, 100);
+
+            Console.WriteLine("\nИсходный массив:");
+            PrintArr(arr, size);
+            Console.WriteLine("\nИтоговый массив:");
+            Shuffle(arr, size);
+            PrintArr(arr, size);
+        }
+        static void PrintArr(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(arr[i]+" ");
+            Console.Write("\n");
+        }
+        static void Shuffle(int[] arr, int size)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                int j = rnd.Next(0, i+1);
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
